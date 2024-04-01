ead8eba7ed59553210c26a8f363981a958f22c85
Joao Gabriel Nascimento Moraes <swokygithub@gmail.com>
2024-04-01 16:52:10 -0500

    Remove blast folder from version control

--- additional_filtering_blast/exported-feature-table/BLAST_NCBI.sh
+++
@@ -1,3 +0,0 @@
-#Sequences filtered-out during quality control (dna-sequences.fasta) were investigated using NCBI BLAST nucleotide database
-
-blastn -db nt -query dna-sequences.fasta -out blast_results.out -remote
